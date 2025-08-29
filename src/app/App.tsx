012e339f6f5c2bb1bc287ee355e26718a941d17f
dnemirich <dnemirich@gmail.com>
2025-08-29 21:42:56 +0200

    feat: add mutation observer task

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,15 @@
-import { Game } from 'entities/game';
-import { createGameModel } from 'entities/game';
+import { LazyImagesLoading } from 'features/intersection-observer';
+import { TodolistWrapper } from 'features/mutation-observer';
+import { BlockSizeCalculation } from 'features/resize-observer';
 
 import s from './App.module.scss';
 
 function App() {
-  const game1 = createGameModel('game1');
-  const game2 = createGameModel('game2');
-
   return (
     <div className={s.container}>
-      <Game model={game1} />
-      <Game model={game2} />
+      <LazyImagesLoading />
+      <BlockSizeCalculation />
+      <TodolistWrapper />
     </div>
   );
 }
---
+++ src/features/mutation-observer/index.ts
@@ -0,0 +1,1 @@
+export { TodolistWrapper } from './TodolistWrapper';
